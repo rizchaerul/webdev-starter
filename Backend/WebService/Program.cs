d154324c06158a69d5460154995b8a8787256c1c
Chaerul Rizky <riz.chaerul@gmail.com>
2022-12-20 14:44:57 +0700

    chore: configure EFCore

--- Backend/WebService/Program.cs
+++ Backend/WebService/Program.cs
@@ -1,13 +1,26 @@
+using Database.Entities;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSwaggerDocument();
 builder.Services.AddRazorPages();
+
+// Add Database Context.
+builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+{
+    // Set default tracking behavior to AsNoTracking()
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    options.UseNpgsql(configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
 
